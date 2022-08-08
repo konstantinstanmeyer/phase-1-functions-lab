7cc319dc8c982bec55a817c8b110fc0ce7241e01
KonstantinStanmeyer <konstantin@stanmeyer.com>
2022-08-08 17:43:12 -0600

    commit tree 286e6389262c2563cc4e4659d59dd802059fab92

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if (block > 42) {
+        return (block - 42);
+    } else {
+        return (42 - block);
+    }
+}
+
+function distanceFromHqInFeet(feet) {
+    return distanceFromHqInBlocks(feet) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return ((start - destination)*264);
+    } else {
+        return ((destination - start)*264);
+    }
+
+}
